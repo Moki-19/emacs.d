d8526b51ff056a513654744db64251148c2046ef
Loic Lemaitre <loic.lemaitre@doqboard.com>
2020-06-15 21:28:53 +0200

    Change magit fetch all shorcut.

--- init.el
+++ init.el
@@ -629,7 +629,7 @@
          ("C-c m f w" . magit-find-file-other-window)
          ("C-c m r r" . magit-reset)
          ("C-c m t" . magit-tag)
-         ("C-c f a" . magit-fetch-all)))
+         ("C-c m f a" . magit-fetch-all)))
 
 ;;--------------------------------------------------------------------------------------------------
 ;; TRAMP
